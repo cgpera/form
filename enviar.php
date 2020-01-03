aedbd571defa953ba27b6ee77f1fbb19ce369b15
Claudio Pera <cgpera@gmail.com>
2020-01-03 12:14:44 -0300

    index3

--- enviar.php
+++ enviar.php
@@ -1,17 +1,9 @@
 <?php
-ini_set("include_path", '/home/allbacte/php:' . ini_get("include_path") );
-include("Mail.php"); # Archivo interno del servidor
-$recipients = "cgpera@gmail.com"; # Mail del receptor del correo
-$headers["From"] = "info@allbacter.com.ar"; # Cuenta de correo valida del dominio
-$headers["To"] = "info@allbacter.com.ar"; # Destinatario del correo
-$headers["Subject"] = "Prueba"; # Asunto de mail
-$mailbody = "Hola esto es una prueba."; # Cuerpo del mail
-$smtpinfo["host"] = "smtp.gmail.com"; # Servidor SMTP
-$smtpinfo["port"] = "587";
-$smtpinfo["auth"] = true;
-$smtpinfo["username"] = "cgpera@gmail.com"; # Cuenta de correo para autentificar
-$smtpinfo["password"] = "C@mell001"; # Clave de la cuenta de correo
-$mail_object =&Mail::factory("smtp", $smtpinfo);
-$mail_object->send($recipients, $headers, $mailbody);
-echo "el correo fue enviado";
+$to = "cgpera@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: cgpera@gmail.com" . "\r\n" .
+"CC: ";
+
+mail($to,$subject,$txt,$headers);
 ?>
--- enviar2.php
+++ enviar2.php
@@ -1,12 +1,24 @@
 <?php
 //Recibimos los campos del formulario
-$nombre=$_POST[nombre];
-$correo=$_POST[correo];
-$asunto=$_POST[asunto];
-$mensaje=$_POST[mensaje];
+$nombre=$_POST['nombre'];
+$correo=$_POST['correo'];
+$asunto=$_POST['asunto'];
+$mensaje=$_POST['mensaje'];
+
+$header = 'From: '.$correo . "\r\n";
+$header .= "X-Mailer: PHP/" . phpversion() .  "\r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Mensaje enviado por " . $nombre .  ",\r\n";
+$mensaje .= "Su email es: " . $correo .  "\r\n";
+$mensaje .= "Mensaje: " .$_POST['mensaje'] .  "\r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
 //Acomodamos todo para darle orden al cuerpo del mensaje
 $texto_mensaje="El usuario ".$nombre." dijo: \n".$mensaje." \n
 Puedes contactarlo a su correo: ".$correo;
-mail("micorreo@ejemplo.com",$asunto,$texto_mensaje);
+mail("cgpera@gmail.com",$asunto,utf8_decode($texto_mensaje), $header);
+header("Location: index3.html");
 echo "Su mensaje se ha enviado! Nos pondremos en contacto contigo.";
 ?>
