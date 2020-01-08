e7b147d9201fbb081160ea42e5c5ff0a6de86fc8
Claudio Gabriel Pera <cgpera@gmail.com>
2020-01-08 18:24:02 -0300

    cambios en send

--- enviar2.php
+++ enviar2.php
@@ -1,8 +1,9 @@
 <?php
 //Recibimos los campos del formulario
+// if(isset($_POST["nombre"]) && isset($_POST["correo"]) && isset($_POST['asunto']) && isset($_POST["comentario"]) ){
 $nombre=$_POST['nombre'];
 $correo=$_POST['correo'];
-$asunto=$_POST['asunto'];
+$telefono=$_POST['telefono'];
 $mensaje=$_POST['mensaje'];
 
 $header = 'From: '.$correo . "\r\n";
@@ -18,7 +19,10 @@ $mensaje .= "Enviado el " . date('d/m/Y', time());
 //Acomodamos todo para darle orden al cuerpo del mensaje
 $texto_mensaje="El usuario ".$nombre." dijo: \n".$mensaje." \n
 Puedes contactarlo a su correo: ".$correo;
-mail("cgpera@gmail.com",$asunto, utf8_decode($mensaje), $header);
-//echo "Su mensaje se ha enviado! Nos pondremos en contacto contigo.";
-header("Location: index3.html");
+if(mail("info@allbacter.com.ar",$telefono, utf8_decode($mensaje), $header)){
+echo "Su mensaje se ha enviado! Nos pondremos en contacto contigo.";
+sleep(3);
+header("Location: index4.html");
+}
+// }
 ?>
